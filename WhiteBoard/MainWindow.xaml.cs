98fe76561ebbc0966f5ae2f88a5e4064825bf942
Marinho Moreira <fmarinho.rodrigues@gmail.com>
2013-01-31 19:32:25 -0700

    connection ok. bugsrelated to draw from last point and draw from message

---
+++ WhiteBoard/MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using GroupLab.iNetwork;
+using GroupLab.iNetwork.Tcp;
+
+namespace WhiteBoard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Class Instance Variables
+        private Connection _connection;
+        #endregion
+
+
+        #region Constructors
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeConnection();
+        }
+        #endregion
+
+
+        #region Initialization
+        private void InitializeConnection()
+        {
+            // Look for a service/connection with the given name. Once found go to event handler.
+            Connection.Discover("WhiteBoardService", new SingleConnectionDiscoveryEventHandler(OnConnectionDiscovered));
+        }
+
+        // Handles the event that a connection is discovered
+        private void OnConnectionDiscovered(Connection connection)
+        {
+            // This connection is the connection that is discorvered
+            this._connection = connection;
+
+            if (this._connection != null)
+            {
+                this._connection.MessageReceived += new ConnectionMessageEventHandler(OnMessageReceived);
+                // Start connection -- networking thread
+                this._connection.Start();
+            }
+            else
+            {
+                // Through the GUI thread, close the window
+                this.Dispatcher.Invoke(
+                    new Action(
+                        delegate()
+                        {
+                            this.Close();
+                        }
+                ));
+            }
+        }
+        #endregion
+
+
+        #region Main Body
+        // Handles the event that a message is received
+        private void OnMessageReceived(object sender, Message msg)
+        {
+            if (msg != null)
+            {
+                // Check message name...
+                switch (msg.Name)
+                {
+                    default:
+                        // Do nothing
+                        break;
+
+                    case "draw":
+                        Point oldP = new Point(msg.GetDoubleField("oldMousePointX"), msg.GetDoubleField("oldMousePointY"));
+                        Point currentP = new Point(msg.GetDoubleField("currentMousePointX"), msg.GetDoubleField("currentMousePointY"));
+                        Console.WriteLine(msg.ToString() + ": " + msg.GetDoubleField("oldMousePointX") + ": " + msg.GetDoubleField("currentMousePointX"));
+                        this.Dispatcher.Invoke(
+                new Action(
+                    delegate()
+                    {
+                        drawFM(oldP, currentP);
+                        
+                    }
+            ));
+                        
+                        break;
+
+                    /*
+                     * The following is a basic example:
+                     * 
+                     * case "ReceivedClientID":
+                     *      msg.AddField("clientID", this.clientID);
+                     *      break;
+                     */
+                }
+            }
+        }
+
+        private void sendMessage(Point oldMousePoint, Point currentMousePoint)
+        {
+            Message msg = new Message("draw");
+            msg.AddField("oldMousePointX", oldMousePoint.X);
+            msg.AddField("oldMousePointY", oldMousePoint.Y);
+            msg.AddField("currentMousePointX", currentMousePoint.X);
+            msg.AddField("currentMousePointY", currentMousePoint.Y);
+            if (this._connection != null)
+                this._connection.SendMessage(msg);
+            //Console.WriteLine(msg.ToString() + ": " + msg.GetDoubleField("oldMousePointX") + ": " + msg.GetDoubleField("currentMousePointX"));
+        }
+
+        #endregion
+
+
+        #region Draw stuff
+
+        Point oldMousePoint;
+
+        private void myCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point currentMousePoint = e.GetPosition((IInputElement)sender);
+                draw(this.oldMousePoint, currentMousePoint);
+                sendMessage(this.oldMousePoint, currentMousePoint);
+            }
+        }
+
+        private void drawFM(Point oldP, Point newP)
+        {
+            Line line = new Line();
+            line.Stroke = Brushes.Blue;
+            line.StrokeThickness = 1;
+            line.X1 = oldP.X;
+            line.Y1 = oldP.Y;
+            line.X2 = newP.X;
+            line.Y2 = newP.Y;
+
+            _mainContainer.Children.Add(line);
+            //this.oldMousePoint = newP;
+        }
+
+
+        private void draw(Point oldP, Point newP)
+        {
+
+            Line line = new Line();
+            line.Stroke = Brushes.Black;
+            line.StrokeThickness = 1;
+            line.X1 = oldP.X;
+            line.Y1 = oldP.Y;
+            line.X2 = newP.X;
+            line.Y2 = newP.Y;
+
+            _mainContainer.Children.Add(line);
+            this.oldMousePoint = newP;
+            
+        }
+
+        private void myCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.oldMousePoint = e.GetPosition((IInputElement)sender);
+        }
+        #endregion
+
+    }
+}
---
+++ WhiteBoardServer/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using GroupLab.iNetwork;
+using GroupLab.iNetwork.Tcp;
+
+namespace WhiteBoardServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Class Instance Variables
+        private Server _server;
+        private List<Connection> _clients;
+        #endregion
+
+
+        #region Constructors
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeServer();
+
+            this.Closing += new CancelEventHandler(OnWindowClosing);
+        }
+        #endregion
+
+
+        #region Initialization
+        private void InitializeServer()
+        {
+            this._clients = new List<Connection>();
+
+            // Creates a new server with the given name and is bound to the given port.
+            this._server = new Server("WhiteBoardService", 12345);
+            this._server.IsDiscoverable = true;
+            this._server.Connection += new ConnectionEventHandler(OnServerConnection);
+            this._server.Start();
+
+            this._statusLabel.Content = "IP: " + this._server.Configuration.IPAddress.ToString()
+                + ", Port: " + this._server.Configuration.Port.ToString();
+        }
+
+        // Handles the event that a connection is made
+        private void OnServerConnection(object sender, ConnectionEventArgs e)
+        {
+            if (e.ConnectionEvent == ConnectionEvents.Connect)
+            {
+                // Lock the list so that only the networking thread affects it
+                lock (this._clients)
+                {
+                    if (!(this._clients.Contains(e.Connection)))
+                    {
+                        // Add to list and create event listener
+                        this._clients.Add(e.Connection);
+                        e.Connection.MessageReceived += new ConnectionMessageEventHandler(OnConnectionMessage);
+
+                        // Using the GUI thread make changes
+                        this.Dispatcher.Invoke(
+                            new Action(
+                                delegate()
+                                {
+                                    this._clientsList.Items.Add(e.Connection);
+                                }
+                        ));
+                    }
+                }
+            }
+            else if (e.ConnectionEvent == ConnectionEvents.Disconnect)
+            {
+                // Lock the list so that only the networking thread affects it
+                lock (this._clients)
+                {
+                    if (this._clients.Contains(e.Connection))
+                    {
+                        // Clean up --  remove from list and remove event listener
+                        this._clients.Remove(e.Connection);
+                        e.Connection.MessageReceived -= new ConnectionMessageEventHandler(OnConnectionMessage);
+
+                        // Using the GUI thread make changes
+                        this.Dispatcher.Invoke(
+                            new Action(
+                                delegate()
+                                {
+                                    this._clientsList.Items.Remove(e.Connection);
+                                }
+                        ));
+                    }
+                }
+            }
+        }
+        #endregion
+
+
+        #region Main Body
+        // Handles the event that a message is received
+        private void OnConnectionMessage(object sender, Message msg)
+        {
+            if (msg != null)
+            {
+                // Check message name...
+                switch (msg.Name)
+                {
+                    default:
+                        //Do nothing
+                        break;
+
+                    case "draw":
+                        this._server.BroadcastMessage(msg);
+                        Console.WriteLine(msg.ToString() + " SERVER: " + msg.GetDoubleField("oldMousePointX") + ": " + msg.GetDoubleField("currentMousePointX"));
+                        break;
+                }
+            }
+        }
+
+        // When server window closes, stop running the server
+        private void OnWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this._server != null && this._server.IsRunning)
+            {
+                this._server.Stop();
+            }
+        }
+        #endregion
+    }
+}
